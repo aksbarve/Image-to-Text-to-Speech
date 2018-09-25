95ebdc946457eab125bfd3dd3684d20077829e1d
aksbarve <aksbarve@gmail.com>
2018-09-25 00:16:22 -0500

    Change for output

--- imgtotexttospeech.py
+++ imgtotexttospeech.py
@@ -3,19 +3,30 @@ import pytesseract
 from gtts import gTTS
 from playsound import playsound
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
-im = Image.open("sample.jpg")
 
+# Tesseract path to downloaded directory. Add to path variable
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+
+# Open the image and get text from the image using tesseract
+# select the image you want here
+im = Image.open("sample.jpg")
 text = pytesseract.image_to_string(im, lang='eng')
 
 print(text)
 
+# Store output as a txt file
+f = open("output.txt", "w+")
+f.write(text)
+
+# Text to speech using gTTS and save the speech as mp3
 language = 'en'
 
 myobj = gTTS(text=text, lang=language, slow=False)
-
 myobj.save("sample.mp3")
 
+# Play mp3 from terminal
 playsound('sample.mp3')
+
 
 
